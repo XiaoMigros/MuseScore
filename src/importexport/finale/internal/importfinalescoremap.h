0f92b3f5b06b43ab87794c82e52b2feda9627eb5
Robert Patterson <robert@robertgpatterson.com>
2025-05-05 14:59:02 -0500

    first round of changes for styles

--- src/importexport/finale/internal/importfinalescoremap.h
+++ src/importexport/finale/internal/importfinalescoremap.h
@@ -25,7 +25,7 @@
 #include <unordered_map>
 #include <memory>
 
-#include "engraving/engravingerrors.h"
+#include "style/style.h"
 
 #include "musx/musx.h"
 
@@ -67,6 +67,8 @@ private:
     void importParts();
     void importMeasures();
     void importBrackets();
+
+    void importStyles(engraving::MStyle& style, musx::dom::Cmper partId);
 
     engraving::Staff* createStaff(engraving::Part* part, const std::shared_ptr<const musx::dom::others::Staff> musxStaff,
                                   const engraving::InstrumentTemplate* it = nullptr);
