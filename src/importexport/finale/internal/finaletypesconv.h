d81e8c6c6b1a70df6e1ecd4eb17813fea3dcf1c9
XiaoMigros <xiaomigros3@gmail.com>
2025-06-21 13:43:02 +0200

    texts part 1

--- src/importexport/finale/internal/finaletypesconv.h
+++ src/importexport/finale/internal/finaletypesconv.h
@@ -58,6 +58,8 @@ public:
     static engraving::KeyMode keyModeFromDiatonicMode(music_theory::DiatonicMode diatonicMode);
     static engraving::SymId acciSymbolFromAcciAmount(int acciAmount);
     static engraving::StaffGroup staffGroupFromNotationStyle(musx::dom::others::Staff::NotationStyle notationStyle);
+    static engraving::String metaTagFromFileInfo(musx::dom::texts::FileInfoText::TextType textType);
+    static engraving::String metaTagFromTextComponent(std::string component);
     static double doubleFromEvpu(musx::dom::Evpu evpu);
     static engraving::PointF evpuToPointF(musx::dom::Evpu xEvpu, musx::dom::Evpu yEvpu);
     static double doubleFromEfix(musx::dom::Efix efix);
