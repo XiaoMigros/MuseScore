e0b5b279396ce2d011c25fd25f8b3fa9e250b781
XiaoMigros <xiaomigros3@gmail.com>
2025-06-28 20:40:05 +0200

    bracket paths part 3

--- src/notation/view/styledialog/bracedesignercanvas.cpp
+++ src/notation/view/styledialog/bracedesignercanvas.cpp
@@ -23,10 +23,13 @@
 #include "bracedesignercanvas.h"
 
 #include <cmath>
+// #include <algorithm>
 
 #include <QPainterPath>
 
-#include "engraving/dom/utils.h"
+// #include "engraving/dom/utils.h"
+
+#include "uicomponents/view/itemmultiselectionmodel.h"
 
 #include "translation.h"
 #include "log.h"
@@ -59,13 +62,13 @@ BraceDesignerCanvas::BraceDesignerCanvas(QQuickItem* parent)
         update();
     });
 
-    uiConfig()->fontChanged().onNotify(this, [this]() { // needed?
-        update();
-    });
+    // uiConfig()->fontChanged().onNotify(this, [this]() { // needed?
+        // update();
+    // });
 
-    connect(this, &BraceDesignerCanvas::enabledChanged, [this](){
-        update();
-    });
+    // connect(this, &BraceDesignerCanvas::enabledChanged, [this](){
+        // update();
+    // });
 
     connect(this, &BraceDesignerCanvas::showOutlineChanged, [this](){
         update();
@@ -73,12 +76,40 @@ BraceDesignerCanvas::BraceDesignerCanvas(QQuickItem* parent)
 
     setScale(QSizeF(1.0, 1.0)); // for now
 
+    connect(this, &BraceDesignerCanvas::scaleChanged, [this](){
+        update();
+    });
+
     qApp->installEventFilter(this);
+
+
+    // m_partsNotifyReceiver = std::make_shared<muse::async::Asyncable>();
+
+    m_selectionModel = new ItemMultiSelectionModel(this);
+    m_selectionModel->setAllowedModifiers(Qt::ShiftModifier);
+
+    connect(m_selectionModel, &ItemMultiSelectionModel::selectionChanged,
+            this, [this](const QItemSelection& selected, const QItemSelection& deselected) {
+        setItemsSelected(deselected.indexes(), false);
+        setItemsSelected(selected.indexes(), true);
+        // do something with the selected/deselected items
+        // emit selectionChanged
+    });
+
+    connect(this, &BraceDesignerCanvas::rowsInserted, this, [this]() {
+        updateRemovingAvailability();
+    });
+
+    onNotationChanged();
+     context()->currentNotationChanged().onNotify(this, [this]() {
+        onNotationChanged();
+    });
 }
 
 BraceDesignerCanvas::~BraceDesignerCanvas()
 {
     muse::DeleteAll(m_pointsAccessibleItems);
+    deleteItems(); //needed?
 }
 
 QVariant BraceDesignerCanvas::pointList() const
@@ -89,6 +120,36 @@ QVariant BraceDesignerCanvas::pointList() const
 QSizeF BraceDesignerCanvas::scale() const
 {
     return m_scale;
+}
+
+void BraceDesignerCanvas::onNotationChanged()
+{
+    // m_partsNotifyReceiver->disconnectAll();
+
+    if (m_isLoadingBlocked) {
+        m_notationChangedWhileLoadingWasBlocked = true;
+        return;
+    }
+
+    onBeforeChangeNotation();
+    m_notation = context()->currentNotation();
+
+    if (m_notation) {
+        load();
+    } else {
+        clear();
+    }
+
+    m_notationChangedWhileLoadingWasBlocked = false;
+}
+
+void BraceDesignerCanvas::onBeforeChangeNotation()
+{
+    if (!m_notation) {
+        return;
+    }
+	QSizeF newScale = QSizeF(m_notation->style()->styleValue(StyleId::akkoladeWidth).toDouble(), m_scale.height());
+	setScale(newScale);
 }
 
 bool BraceDesignerCanvas::showOutline() const
@@ -241,7 +302,11 @@ void BraceDesignerCanvas::setShowOutline(bool show)
 void BraceDesignerCanvas::setPointList(QVariant points)
 {
     BracePoints newPointList = bracePointsFromQVariant(points);
+    setPointListInternal(newPointList);
+}
 
+void BraceDesignerCanvas::setPointListInternal(const BracePoints& newPointList)
+{
     if (m_points == newPointList) {
         return;
     }
@@ -627,7 +692,7 @@ bool BraceDesignerCanvas::movePoint(int pointIndex, BracePoint toPoint)
 
     // lock start and end point to the vertical center.
     // This is so we get a closed bath (upon mirroring)
-    if (pointIndex == 0 || pointIndex == m_points.size()) {
+    if (isFirstOrLastPoint(pointIndex)) {
         toPoint.relative.ry() = 0.0;
         toPoint.absolute.ry() = 0.0;
     }
@@ -677,4 +742,423 @@ void BraceDesignerCanvas::setAccessibleParent(muse::ui::AccessibleItem* parent)
     }
 
     emit accessibleParentChanged();
+}
+
+bool BraceDesignerCanvas::isFirstOrLastPoint(int index)
+{
+    return index == 0 || index == m_points.size() - 1;
+}
+
+bool BraceDesignerCanvas::removeRows(int row, int count, const QModelIndex& parent)
+{
+    if (!m_isRemovingAvailable) {
+        return false;
+    }
+
+    // BraceDesignerCanvas* parentItem = modelIndexToItem(parent);
+
+    // if (!parentItem) {
+        // parentItem = m_rootItem;
+    // }
+
+    setLoadingBlocked(true);
+    beginRemoveRows(parent, row, row + count - 1);
+
+    // parentItem->removeChildren(row, count, true);
+
+    endRemoveRows();
+    setLoadingBlocked(false);
+
+    emit isEmptyChanged();
+
+    return true;
+}
+
+void BraceDesignerCanvas::clear()
+{
+    // TRACEFUNC;
+
+    // beginResetModel();
+    // deleteItems();
+    // endResetModel();
+
+    // emit isEmptyChanged();
+    // emit isAddingAvailableChanged(false);
+}
+
+void BraceDesignerCanvas::deleteItems()
+{
+    clearSelection();
+    setPointListInternal(BracePoints());
+}
+
+void BraceDesignerCanvas::load()
+{
+    if (m_isLoadingBlocked) {
+        return;
+    }
+
+    TRACEFUNC;
+
+    beginResetModel();
+
+    clearSelection(); // since points are already bound and we don't need to collect from engraving, this should be fine???
+
+    endResetModel();
+
+
+    emit isEmptyChanged();
+    // emit isAddingAvailableChanged(true);
+}
+
+QVariant BraceDesignerCanvas::data(const QModelIndex& index, int role) const
+{
+    int row = index.row();
+
+    if (!isPointIndexValid(row) && role != ItemRole) {
+        return QVariant();
+    }
+
+    return m_points.at(row).toMap(); // is this a qvariant?
+}
+
+bool BraceDesignerCanvas::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+    int row = index.row();
+
+    if (!isPointIndexValid(row) && role != ItemRole) {
+        return false;
+    }
+
+    BracePoints points = m_points;
+    BracePoint& point = points.at(row);
+    point = BracePoint::fromMap(value);
+    setPointListInternal(points);
+    clearSelection();
+    m_selection->select(index);
+    emit dataChanged(index, index, { ItemRole }); // needed?
+    emit selectionChanged();
+    return true;
+}
+
+int BraceDesignerCanvas::rowCount(const QModelIndex&) const
+{
+    return m_points.size();
+}
+
+void BraceDesignerCanvas::selectRow(const QModelIndex& rowIndex)
+{
+    m_selectionModel->select(rowIndex);
+}
+
+void BraceDesignerCanvas::clearSelection()
+{
+    m_selectionModel->clear();
+}
+
+void BraceDesignerCanvas::moveSelectedRowsUp()
+{
+    if (!m_isMovingUpAvailable) {
+        return;
+    }
+
+    QModelIndexList selectedIndexList = m_selectionModel->selectedIndexes();
+    if (selectedIndexList.isEmpty()) {
+        return;
+    }
+
+    std::sort(selectedIndexList.begin(), selectedIndexList.end(), [](QModelIndex f, QModelIndex s) -> bool {
+        return f.row() < s.row();
+    });
+
+    const QModelIndex& sourceRowFirst = selectedIndexList.first();
+
+    moveRows(sourceRowFirst.parent(), sourceRowFirst.row(), selectedIndexList.count(), sourceRowFirst.parent(), sourceRowFirst.row() - 1);
+}
+
+void BraceDesignerCanvas::moveSelectedRowsDown()
+{
+    if (!m_isMovingDownAvailable) {
+        return;
+    }
+
+    QModelIndexList selectedIndexList = m_selectionModel->selectedIndexes();
+    if (selectedIndexList.isEmpty()) {
+        return;
+    }
+
+    std::sort(selectedIndexList.begin(), selectedIndexList.end(), [](const QModelIndex& f, const QModelIndex& s) -> bool {
+        return f.row() < s.row();
+    });
+
+    const QModelIndex& sourceRowFirst = selectedIndexList.first();
+    const QModelIndex& sourceRowLast = selectedIndexList.last();
+
+    moveRows(sourceRowFirst.parent(), sourceRowFirst.row(), selectedIndexList.count(), sourceRowFirst.parent(), sourceRowLast.row() + 1);
+}
+
+void BraceDesignerCanvas::removeSelectedRows()
+{
+    if (!m_isRemovingAvailable) {
+        return;
+    }
+
+    QModelIndexList selectedIndexList = m_selectionModel->selectedIndexes();
+    if (selectedIndexList.empty()) {
+        return;
+    }
+
+    QModelIndex firstIndex = *std::min_element(selectedIndexList.cbegin(), selectedIndexList.cend(),
+                                               [](const QModelIndex& f, const QModelIndex& s) {
+        return f.row() < s.row();
+    });
+
+    removeRows(firstIndex.row(), selectedIndexList.size(), firstIndex.parent());
+}
+
+void BraceDesignerCanvas::insertNewItem(int index)
+{
+    // todo: when there's only one point, we should append instead of inserting before it
+    BracePoint p = isPointIndexValid(index) ? m_points.at(index) : BracePoint(BracePoint::PointType::Move, QPointF(), QPointF());
+    if (isPointIndexValid(index + 1)) {
+        p.relative.rx() = (p.relative.x() + m_points.at(index + 1).relative.x()) / 2;
+        p.relative.ry() = (p.relative.y() + m_points.at(index + 1).relative.y()) / 2;
+        p.absolute.rx() = (p.absolute.x() + m_points.at(index + 1).absolute.x()) / 2;
+        p.absolute.ry() = (p.absolute.y() + m_points.at(index + 1).absolute.y()) / 2;
+    }
+    BracePoints list = m_points;
+    list.insert(index, p);
+    setPointListInternal(list);
+}
+
+int BraceDesignerCanvas::indexOf(const BracePoint& point) const
+{
+    return m_points.indexOf(point);
+}
+
+bool BraceDesignerCanvas::moveRows(const QModelIndex& sourceParent, int sourceRow, int count, const QModelIndex& destinationParent,
+                                    int destinationChild)
+{
+    setLoadingBlocked(true);
+    DEFER {
+        setLoadingBlocked(false);
+    };
+
+    // QVariantMap sourceItem = modelIndexToItem(sourceParent);
+    // QVariantMap sourceItem = modelIndexToItem(destinationParent);
+
+    int sourceFirstRow = sourceRow;
+    int sourceLastRow = sourceRow + count - 1;
+    int destinationRow = /*(*/sourceLastRow > destinationChild /*|| sourceParentItem != destinationParentItem)*/
+                         ? destinationChild : destinationChild + 1;
+
+    /* if (m_dragInProgress) {
+        MoveParams moveParams;
+        moveParams.sourceRow = sourceRow;
+        moveParams.count = count;
+        moveParams.destinationRow = destinationRow;
+
+        if (!params.isValid()) {
+            return false;
+        }
+
+        // m_dragSourceParentItem = sourceParentItem;
+        m_activeDragMoveParams = params;
+    } */
+
+    beginMoveRows(sourceParent, sourceFirstRow, sourceLastRow, destinationParent, destinationRow);
+    // sourceParentItem->moveChildren(sourceFirstRow, count, destinationParentItem, destinationRow, !m_dragInProgress);
+    endMoveRows(); // if nothing needed, replace with moverows
+    updateRearrangementAvailability();
+
+    return true;
+}
+
+QVariantMap BraceDesignerCanvas::modelIndexToItem(const QModelIndex& index) const
+{
+    return data(index, ItemRole);
+}
+
+void BraceDesignerCanvas::startActiveDrag()
+{
+    m_dragInProgress = true;
+}
+
+void BraceDesignerCanvas::endActiveDrag()
+{
+    setLoadingBlocked(true);
+
+    // here we would commit the change, but since we're only working with the qt model we can do nothing here instead
+
+    // m_dragSourceParentItem = nullptr;
+    // m_activeDragMoveParams = MoveParams();
+    m_dragInProgress = false;
+
+    setLoadingBlocked(false);
+
+    // updateRearrangementAvailability(); //perhaps needed? not in the original code.
+}
+
+QItemSelectionModel* BraceDesignerCanvas::selectionModel() const
+{
+    return m_selectionModel;
+}
+
+/*QModelIndex BraceDesignerCanvas::index(int row, int column, const QModelIndex& parent) const
+{
+    if (!hasIndex(row, column, parent)) {
+        return QModelIndex();
+    }
+
+    const BraceDesignerCanvas* parentItem = nullptr;
+
+    if (!parent.isValid()) {
+        parentItem = m_rootItem;
+    } else {
+        parentItem = modelIndexToItem(parent);
+    }
+
+    if (!parentItem) {
+        return QModelIndex();
+    }
+
+    BraceDesignerCanvas* childItem = parentItem->childAtRow(row);
+
+    if (childItem) {
+        return createIndex(row, column, childItem);
+    }
+
+    return QModelIndex();
+}*/
+
+int BraceDesignerCanvas::columnCount(const QModelIndex&) const
+{
+    return 1;
+}
+
+void BraceDesignerCanvas::setIsMovingUpAvailable(bool isMovingUpAvailable)
+{
+    if (m_isMovingUpAvailable == isMovingUpAvailable) {
+        return;
+    }
+
+    m_isMovingUpAvailable = isMovingUpAvailable;
+    emit isMovingUpAvailableChanged(m_isMovingUpAvailable);
+}
+
+void BraceDesignerCanvas::setIsMovingDownAvailable(bool isMovingDownAvailable)
+{
+    if (m_isMovingDownAvailable == isMovingDownAvailable) {
+        return;
+    }
+
+    m_isMovingDownAvailable = isMovingDownAvailable;
+    emit isMovingDownAvailableChanged(m_isMovingDownAvailable);
+}
+
+bool BraceDesignerCanvas::isMovingUpAvailable() const
+{
+    return m_isMovingUpAvailable;
+}
+
+bool BraceDesignerCanvas::isMovingDownAvailable() const
+{
+    return m_isMovingDownAvailable;
+}
+
+bool BraceDesignerCanvas::isRemovingAvailable() const
+{
+    return m_isRemovingAvailable;
+}
+
+bool BraceDesignerCanvas::isEmpty() const
+{
+    return m_rootItem ? m_rootItem->isEmpty() : true;
+}
+
+void BraceDesignerCanvas::updateRearrangementAvailability()
+{
+    QModelIndexList selectedIndexList = m_selectionModel->selectedIndexes();
+
+    if (selectedIndexList.isEmpty()) {
+        updateMovingUpAvailability(false);
+        updateMovingDownAvailability(false);
+        return;
+    }
+
+    std::sort(selectedIndexList.begin(), selectedIndexList.end(), [](const QModelIndex& f, const QModelIndex& s) -> bool {
+        return f.row() < s.row();
+    });
+
+    bool isRearrangementAvailable = true;
+
+    QMutableListIterator<QModelIndex> it(selectedIndexList);
+
+    while (it.hasNext() && selectedIndexList.count() > 1) {
+        int nextRow = it.next().row();
+        int previousRow = it.peekPrevious().row();
+
+        isRearrangementAvailable = (nextRow - previousRow == 1);
+        if (!isRearrangementAvailable) {
+            updateMovingUpAvailability(isRearrangementAvailable);
+            updateMovingDownAvailability(isRearrangementAvailable);
+            return;
+        }
+    }
+
+    updateMovingUpAvailability(isRearrangementAvailable, selectedIndexList.first());
+    updateMovingDownAvailability(isRearrangementAvailable, selectedIndexList.last());
+}
+
+void BraceDesignerCanvas::updateMovingUpAvailability(bool isSelectionMovable, const QModelIndex& firstSelectedRowIndex)
+{
+    bool isRowInBoundaries = firstSelectedRowIndex.isValid() && firstSelectedRowIndex.row() > 0;
+    setIsMovingUpAvailable(isSelectionMovable && isRowInBoundaries);
+}
+
+void BraceDesignerCanvas::updateMovingDownAvailability(bool isSelectionMovable, const QModelIndex& lastSelectedRowIndex)
+{
+    bool isRowInBoundaries = lastSelectedRowIndex.isValid() && lastSelectedRowIndex.row() + 1 < m_points.size();
+    setIsMovingDownAvailable(isSelectionMovable && isRowInBoundaries);
+}
+
+void BraceDesignerCanvas::updateRemovingAvailability()
+{
+    const QModelIndexList selectedIndexes = m_selectionModel->selectedIndexes();
+
+    bool isRemovingAvailable = !selectedIndexes.empty();
+
+    for (const QModelIndex& index : selectedIndexes) {
+        isRemovingAvailable = isRemovingAvailable && !isFirstOrLastPoint(index.row());
+
+        if (!isRemovingAvailable) {
+            break;
+        }
+    }
+
+    setIsRemovingAvailable(isRemovingAvailable);
+}
+
+void BraceDesignerCanvas::setItemsSelected(const QModelIndexList& indexes, bool selected)
+{
+    for (const QModelIndex& index : indexes) {
+        m_selection->select(modelIndex, selected ? QItemSelectionModel::Select : QItemSelectionModel::Deselect);
+    }
+}
+
+QModelIndex BraceDesignerCanvas::index(int row, int column, const QModelIndex& model) const
+{
+    if (hasIndex(row, column, model && isPointIndexValid(row)) {
+        return createIndex(row, column, m_points.at(row).toMap());
+    }
+    return QModelIndex();
+}
+
+void BraceDesignerCanvas::setLoadingBlocked(bool blocked)
+{
+    m_isLoadingBlocked = blocked;
+
+    if (!m_isLoadingBlocked && m_notationChangedWhileLoadingWasBlocked) {
+        onNotationChanged();
+    }
 }
