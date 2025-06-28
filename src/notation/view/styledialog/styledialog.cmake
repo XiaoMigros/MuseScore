52cc9ed433cfc6d78750623ded84deeb249b96c1
XiaoMigros <xiaomigros3@gmail.com>
2025-06-29 01:39:11 +0200

    brackets part 3.5

--- src/notation/view/styledialog/styledialog.cmake
+++ src/notation/view/styledialog/styledialog.cmake
@@ -14,6 +14,8 @@ set(STYLEDIALOG_SRC
     ${CMAKE_CURRENT_LIST_DIR}/bendstyleselector.h
     ${CMAKE_CURRENT_LIST_DIR}/bracedesignersectionmodel.cpp
     ${CMAKE_CURRENT_LIST_DIR}/bracedesignersectionmodel.h
+    ${CMAKE_CURRENT_LIST_DIR}/bracedesignerpointsmodel.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/bracedesignerpointsmodel.h
     ${CMAKE_CURRENT_LIST_DIR}/bracedesignercanvas.cpp
     ${CMAKE_CURRENT_LIST_DIR}/bracedesignercanvas.h
     ${CMAKE_CURRENT_LIST_DIR}/tieplacementselector.cpp
