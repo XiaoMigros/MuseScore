d08bb8fdfcf0412f2a09cc4f27f61af00e91aba7
XiaoMigros <xiaomigros3@gmail.com>
2025-07-22 03:23:00 +0200

    Add some measure properties

--- src/importexport/finale/internal/importfinaleparser.h
+++ src/importexport/finale/internal/importfinaleparser.h
@@ -220,7 +220,8 @@ private:
     void importClefs(const std::shared_ptr<musx::dom::others::InstrumentUsed>& musxScrollViewItem,
                      const std::shared_ptr<musx::dom::others::Measure>& musxMeasure,
                      engraving::Measure* measure, engraving::staff_idx_t curStaffIdx,
-                     musx::dom::ClefIndex& musxCurrClef);
+                     musx::dom::ClefIndex& musxCurrClef,
+                     const std::shared_ptr<musx::dom::others::Measure>& nextMusxMeasure);
     bool applyStaffSyles(engraving::StaffType* staffType, const std::shared_ptr<const musx::dom::others::StaffComposite>& currStaff);
 
     // entries
